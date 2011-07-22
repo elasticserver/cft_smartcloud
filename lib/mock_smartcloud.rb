393e68adbbc3d52227a88c5fcd422509c9b4a23e
yan <yan@pritzker.ws>
2011-07-22 15:10:57 -0700

    Added license/copyright info

--- lib/mock_smartcloud.rb
+++ lib/mock_smartcloud.rb
@@ -1,3 +1,15 @@
+#!/usr/bin/env ruby
+#####################################################################################
+# Copyright (c) 2011, Cohesive Flexible Technologies, Inc.
+# This copyrighted material is the property of Cohesive Flexible Technologies and
+# is subject to the license terms of the product it is contained within, whether 
+# in text or compiled form.  It is licensed under the terms expressed in the 
+# accompanying README and LICENSE files.
+# 
+# This program is AS IS and WITHOUT ANY WARRANTY; without even the implied warranty 
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#####################################################################################
+
 class MockSmartCloud
   def create_instance(*args)
     describe_instance
--- lib/smartcloud.rb
+++ lib/smartcloud.rb
@@ -1,3 +1,15 @@
+#!/usr/bin/env ruby
+#####################################################################################
+# Copyright (c) 2011, Cohesive Flexible Technologies, Inc.
+# This copyrighted material is the property of Cohesive Flexible Technologies and
+# is subject to the license terms of the product it is contained within, whether 
+# in text or compiled form.  It is licensed under the terms expressed in the 
+# accompanying README and LICENSE files.
+# 
+# This program is AS IS and WITHOUT ANY WARRANTY; without even the implied warranty 
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#####################################################################################
+
 Dir["#{File.dirname(__FILE__)}/**/**"].each {|dir| $LOAD_PATH << dir }
 $LOAD_PATH << File.dirname(__FILE__)
 
